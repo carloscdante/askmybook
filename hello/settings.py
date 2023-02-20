78a693a81a37a1324da455c72efa0e4e32f876cb
Carlos Dante <contato.carloscdante@gmail.com>
2023-02-20 14:49:01 -0300

    fix: 🐛 fixed prompt

--- hello/settings.py
+++ hello/settings.py
@@ -14,7 +14,7 @@ IS_HEROKU = "DYNO" in os.environ
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead)."
+SECRET_KEY = "S3CRET"
 
 if 'SECRET_KEY' in os.environ:
     SECRET_KEY = os.environ["SECRET_KEY"]
